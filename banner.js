8bbc16fee8e9d7c77d3b0805e3f8a711bd14030c
Yogaisan Ramasawmy <yogaisan@gmail.com>
2022-08-05 14:47:41 +0400

    Modified profile

---
+++ banner.js
@@ -0,0 +1,15 @@
+const typed = new Typed('#banner-typed-text', {
+    strings: ["I value clarity, empathy, and integrity above everything else.", "I love coding beautiful and simple things."],
+    // typing speed
+    typeSpeed: 60,
+    // backspacing speed
+    backSpeed: 20,
+    // time before backspacing
+    backDelay: 2000,
+    // show cursor
+    showCursor: false,
+    // character for cursor
+    cursorChar: "|",
+    // loop
+    loop: true
+  });
